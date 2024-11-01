5c08fd04a6ae7bfa2802445607fe3d0b409ef0a5
reyhanehkarimi <reyhanehkarimi9978@gmail.com>
2024-11-01 12:59:22 +0330

    write-edit-redux

--- src/main.jsx
+++ src/main.jsx
@@ -3,9 +3,10 @@ import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App.jsx';
 import { Provider } from 'react-redux';
-import store from './redux/store'; 
+import store from './redux/store';
 
-const root = createRoot(document.getElementById('root'));
+const rootElement = document.getElementById('root');
+const root = createRoot(rootElement);
 
 root.render(
   <StrictMode>
